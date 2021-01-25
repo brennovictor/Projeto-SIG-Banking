e5755f30d7d59cdc042d804c30510443324df057
brenno victor victor <brennovictoralves@gmail.com>
2021-01-25 02:49:53 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,42 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                  Projeto Sistema de Gestão Escolar                      ///
+///                Developed by  @flgorgonio - Jan, 2021                    ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void menuSobre(void);
+
+int main(void) {
+    menuSobre();
+    return 0;
+
+}
+
+
+void menuSobre(void) {
+printf("//////////////////////////////////////////////////////////////////////////////////////////\n"
+"///                                                                                    ///\n"
+"///             Universidade Federal do Rio Grande do Norte                            ///\n"
+"///                Centro de Ensino Superior do Seridó                                 ///\n"
+"///               Departamento de Computação e Tecnologia                              ///\n"
+"///                  Disciplina DCT1106 -- Programação                                 ///\n"
+"///                  Projeto Sistema de Gestão Escolar                                 ///\n"
+"///                Developed by  @brennovictor - Jan, 2021                             ///\n"
+"///                                                                                    ///\n"
+"//////////////////////////////////////////////////////////////////////////////////////////\n"
+"///                                                                                    ///\n"
+"///            = = = = = Sistema de Controle de Contas Bancárias = = = = =             ///\n"
+"///                                                                                    ///\n"
+"///  Programa utilizado como parte da avaliação da disciplina DCT1106 - Programação.   ///\n"
+"///  O presente projeto tem como objetivo desenvolver um programa de computador que    ///\n"
+"///  realize o registro e o acompanhamento de operações financeiras de várias contas   ///\n"
+"///  bancárias, simulando as atividades que possam ocorrem em um banco real.           ///\n"
+"///                                                                                    ///\n"
+"//////////////////////////////////////////////////////////////////////////////////////////\n");
+}
+
